74f30241a01aa4b0123436e508a2ece05f4d3c3d
sujoy <sujoydhar95@gmail.com>
2024-03-04 00:11:39 +0600

    new local env

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -5,18 +5,17 @@ def query_wrap_object(template):
   sql = f"""
   (SELECT COALESCE(row_to_json(object_row),'{{}}'::json) FROM (
   {template}
-  ) object_row);
-  """
+  ) object_row);"""
+
   return sql
 
 def query_wrap_array(template):
   sql = f"""
   (SELECT COALESCE(array_to_json(array_agg(row_to_json(array_row))),'[]'::json) FROM (
   {template}
-  ) array_row);
-  """
+  ) array_row);"""
+
   return sql
 
 connection_url = os.getenv("CONNECTION_URL")
 pool = ConnectionPool(connection_url)
-
